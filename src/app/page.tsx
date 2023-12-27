f48a8b97371cb9b8e2d0ad00ceccb1c58f8701b4
sinarck <j7le2w3f@duck.com>
2023-12-27 16:55:15 -0600

    feat: blank app store download button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import AppStore from "../../public/app_store.svg";
+
 const Home = () => {
   return (
     <main className="flex flex-col items-center p-page overflow-hidden">
-      <div className="rounded-full border w-auto py-1 px-3 shadow-sm border-neutral-800 backdrop-blur-sm backdrop-filter bg-neutral-900">
+      <div className="select-none rounded-full border w-auto py-1 px-3 shadow-md border-neutral-800 backdrop-blur-sm backdrop-filter bg-neutral-900">
         <p className="text-xs gradient-text">
           Developed by students, for students
         </p>
@@ -9,10 +12,15 @@ const Home = () => {
       <h1 className="gradient-text select-none tracking-tight w-3/4 text-center text-7xl leading-none pb-2 font-semibold pt-5">
         Voluntra is a better way to volunteer
       </h1>
-      <p className="text-neutral-500">
+      <p className="text-neutral-500 select-none">
         Discover opportunities to volunteer in your community, track your hours,
         and integrate with Xello.
       </p>
+      <Image
+        alt="Download on Apple App Store"
+        src={AppStore}
+        className="cursor-pointer pt-5"
+      />
     </main>
   );
 };
---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <nav className="font-medium w-full border-b border-neutral-800 h-14 flex items-center align-middle text-foreground px-5 backdrop-blur-sm">
+      Voluntra
+    </nav>
+  );
+};
+
+export default NavBar;
