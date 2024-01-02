89df220c1a3a7a1f47434155b7d95f3d2b946f60
Sinarck <83467783+sinarck@users.noreply.github.com>
2024-01-02 16:34:42 -0600

    chore: remove commented code

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -9,13 +9,6 @@ const NavBar = () => {
         <Image src={Logo} alt="Logo" height={24} width={24} />
         <p>Voluntra</p>
       </Link>
-      {/* <div className="bg-transparent/30 rounded-full border border-neutral-800 py-2 px-3 flex flex-row gap-2">
-        {siteConfig.links.map((link) => (
-          <Link href={link.href} key={link.name}>
-            {link.name}
-          </Link>
-        ))}
-      </div> */}
       <div>Voluntra</div>
     </nav>
   );
