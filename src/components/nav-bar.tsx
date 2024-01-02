ce0dcd1d25dc0394018391e9d385e01dc500b998
sinarck <83467783+sinarck@users.noreply.github.com>
2024-01-02 16:13:51 -0600

    feat: add barnding to website

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,20 +1,21 @@
-import siteConfig from "@/config/siteConfig";
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "../app/icon.png";
 
 const NavBar = () => {
   return (
     <nav className="fixed z-50 justify-between w-full border-b border-neutral-800 h-14 flex items-center align-middle text-foreground px-5 backdrop-blur-md">
       <Link className="flex gap-2 font-medium" href="/">
-        <p>A</p>
+        <Image src={Logo} alt="Logo" height={24} width={24} />
         <p>Voluntra</p>
       </Link>
-      <div className="bg-transparent/30 rounded-full border border-neutral-800 py-2 px-3 flex flex-row gap-2">
+      {/* <div className="bg-transparent/30 rounded-full border border-neutral-800 py-2 px-3 flex flex-row gap-2">
         {siteConfig.links.map((link) => (
           <Link href={link.href} key={link.name}>
             {link.name}
           </Link>
         ))}
-      </div>
+      </div> */}
       <div>Voluntra</div>
     </nav>
   );
