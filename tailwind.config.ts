72f515bce75edb463ba9d4a94831fe525c8a3d40
Sinarck <83467783+sinarck@users.noreply.github.com>
2024-01-02 18:28:01 -0600

    feat: use shadcn component library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,74 +1,83 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       padding: {
         page: "6rem",
       },
       colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
         background: "#0F0F0F",
         foreground: "#E0E0E0",
-        white: "#E0E0E0",
-        black: "#0F0F0F",
-        purple: {
-          100: "#E0CCF4",
-          200: "#D4BFEB",
-          300: "#C9B2E2",
-          400: "#BEA6D9",
-          500: "#B29BD1",
-          600: "#A78FC8",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        blue: {
-          100: "#C9D2F8",
-          200: "#B9C4F0",
-          300: "#ABB7E7",
-          400: "#9CAADF",
-          500: "#8F9FD6",
-          600: "#8293CE",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
-        brown: {
-          100: "#EEF0A1",
-          200: "#E2E293",
-          300: "#D4D285",
-          400: "#C6C277",
-          500: "#B8B26B",
-          600: "#ABA45F",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        red: {
-          100: "#FAD1D1",
-          200: "#EEC1C1",
-          300: "#E3B2B2",
-          400: "#D7A3A3",
-          500: "#CC9595",
-          600: "#C08888",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
-        green: {
-          100: "#D3F9B5",
-          200: "#C4EDA5",
-          300: "#B6E095",
-          400: "#A8D487",
-          500: "#9AC779",
-          600: "#8DBA6B",
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
-        neutral: {
-          100: "#C6C6C6",
-          200: "#ACACAC",
-          300: "#929292",
-          400: "#787878",
-          500: "#5D5D5D",
-          600: "#434343",
-          700: "#292929",
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
 
+export default config;
