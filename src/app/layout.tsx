e12c6109459849419c38c85fc8c2433a8d041b70
Sinarck <83467783+sinarck@users.noreply.github.com>
2024-02-12 08:52:18 -0600

    feat: add analytics via vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/footer";
 import NavBar from "@/components/nav-bar";
 import siteConfig from "@/config/siteConfig";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -24,6 +25,7 @@ export default function RootLayout({
         <NavBar />
         <SpeedInsights />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
