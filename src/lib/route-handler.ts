0b18216273e4cb14c06dfc1535ea75aa810f0ab3
Sinarck <83467783+sinarck@users.noreply.github.com>
2024-06-05 16:44:44 -0500

    feat: remove built-in rate limiting in favor of `Vercel Firewall`

--- src/lib/route-handler.ts
+++ src/lib/route-handler.ts
@@ -1,34 +1,9 @@
 // apiHandler.ts
-import { RateLimiter } from "@/lib/rate-limiter";
 import { ApiResponseError } from "@/types/api/response";
 import { NextRequest, NextResponse } from "next/server";
 
-const rateLimiter = new RateLimiter({
-  windowSize: 10000,
-  maxRequests: 10,
-});
-
 export function createHandler(handler: (request: NextRequest) => any) {
   return async (request: NextRequest) => {
-    const ip =
-      request.ip ?? request.headers.get("X-Forwarded-For") ?? "unknown";
-    const isRateLimited = rateLimiter.limit(ip);
-
-    if (isRateLimited) {
-      return NextResponse.json<ApiResponseError>(
-        {
-          message: "Slow down! Too many requests",
-          error: {
-            code: "429",
-            message: "Rate limit exceeded",
-          },
-        },
-        {
-          status: 429,
-        },
-      );
-    }
-
     try {
       return await handler(request);
     } catch (e: any) {
@@ -44,7 +19,7 @@ export function createHandler(handler: (request: NextRequest) => any) {
         },
         {
           status: 500,
-        },
+        }
       );
     }
   };
